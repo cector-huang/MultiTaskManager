ec7101e04d48d1c5e8194ce71155ed119b8d5d80
cector-huang <huangjiaqi@sc.10086.cn.defaultbadlist>
2017-05-18 22:38:12 +0800

    添加maven工程

---
+++ src/main/java/com/cector/multitaskmanager/DBMonitor.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.cector.multitaskmanager;
+
+import org.apache.log4j.Logger;
+
+/**
+ *
+ * @author cector
+ */
+public class DBMonitor implements Runnable{
+    Logger LOGGER = Logger.getLogger(DBMonitor.class);
+    private static int i;
+    
+    DBMonitor()
+    {
+        i = 1;
+    }
+    
+    @Override
+    public void run()
+    {
+        init();
+        while(true)
+        {
+            Utils.sleep(10000);
+            //每过10s钟处理一次数据库
+            processWork();
+                    
+        }
+    }
+    
+    private void processWork()
+    {
+        //查询数据库记录，如果查询到最新数据就把数据库中的新任务添加到任务队列
+        String str = "这是第" + (i++) + "次查询并处理数据库";
+        LOGGER.info(str);
+        //初始化100个线程的线程池
+        //每次操作就添加4个任务到线程池
+        MultiThread.getThreadPool(100).execute(new Runnable[] { new Task(), new Task(), new Task(), new Task()});
+    }
+    
+    private void init()
+    {
+        System.out.println("Enter DBMonitor!");
+    }
+}
---
+++ src/main/java/com/cector/multitaskmanager/ThreadPool.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.cector.multitaskmanager;
+
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
+
+/**
+ *
+ * @author cector
+ */
+public class ThreadPool {
+    
+    Logger LOGGER;
+
+    public ThreadPool() {
+        this.LOGGER = Logger.getLogger(ThreadPool.class);
+        LOGGER.info("begin to create instance!");
+    }
+    
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        //启动数据库监控线程
+        init();
+        Runnable dbmonitor = new DBMonitor();
+        Thread monitor = new Thread(dbmonitor, "DBMonitor");
+        monitor.setDaemon(false);//非守护线程
+        monitor.start();//开始监控数据库，如果数据库中有任何变化就添加到任务队列
+    }
+    
+    private static void init()
+    {
+        String url = ThreadPool.class.getProtectionDomain().getCodeSource().getLocation().getPath();
+        System.out.println(url);
+        String path = "";
+        if(url.endsWith(".jar"))
+        {
+            path = url.substring(0, url.lastIndexOf("/")+1);
+        }
+        if(path != "" && !path.isEmpty())
+        {
+            path += "config/log4j.properties";
+            System.out.println(path);
+            PropertyConfigurator.configure(path);
+        }
+    }
+    
+}
